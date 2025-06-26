7918ce490f27a0997b25bf7edaf0aca993471f30
LuckyEnough2Play <withanyluck@outlook.com>
2025-06-26 07:37:33 -0400

    chore: remove URL polyfill import

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import "react-native-url-polyfill/auto";
 
 export const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL || "",
